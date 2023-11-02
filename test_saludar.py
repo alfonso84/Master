d71c781702af59e8d766536979d7f62501a7b39c
ricardo <minsait8@renta4.es>
2023-11-02 21:57:20 +0100

    add new features

--- test_saludar.py
+++ test_saludar.py
@@ -1,13 +1,30 @@
 import saludar
+import unittest
 
 
-class TestSaludar:
+def saludo1():
+    return "Hola"
+
+
+def saludo2():
+    return "Buenos días"
+
+
+def saludo3():
+    return "Hola, ¿Qué tal?"
+
+
+class TestSaludar(unittest.TestCase):
 
     def test_saludo1(self):
-        assert "Hola" == saludar.saludo1()
+        assert "Hola" == saludar1()
 
     def test_saludo2(self):
-        assert "Buenos días" == saludar.saludo2()
+        assert "Buenos días" == saludar2()
 
     def test_saludo3(self):
-        assert "Hola, ¿Qué tal?" == saludar.saludo3()
+        assert "Hola, ¿Qué tal?" == saludar3()
+
+
+if __name__ == "__main__":
+    unittest.main()
