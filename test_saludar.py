32ca3e9ebeee28cc61c17687ea501e599635ad61
ricardo <minsait8@renta4.es>
2023-11-02 21:50:23 +0100

    add new features

--- test_saludar.py
+++ test_saludar.py
@@ -1,7 +1,8 @@
 import saludar
+import unittest
 
 
-class TestSaludar:
+class TestSaludar(unittest.TestCase):
 
     def test_saludo1(self):
         assert "Hola" == saludar.saludo1()
@@ -11,3 +12,18 @@ class TestSaludar:
 
     def test_saludo3(self):
         assert "Hola, ¿Qué tal?" == saludar.saludo3()
+
+    def test_saludo_con_nombre(self):
+        assert "Hola, Juan" == saludar.saludar("Juan")
+
+    def test_saludo_con_nombre_vacio(self):
+        with self.assertRaises(ValueError):
+            saludar.saludar("")
+
+    def test_saludo_con_nombre_numerico(self):
+        with self.assertRaises(TypeError):
+            saludar.saludar(123)
+
+
+if __name__ == "__main__":
+    unittest.main()
