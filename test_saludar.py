8b6583cb8b63679b7ed1a888f8bdb5e93a01c704
ricardo <minsait8@renta4.es>
2023-11-02 19:54:22 +0100

    add new features

--- test_saludar.py
+++ test_saludar.py
@@ -1,13 +1,25 @@
 import saludar
-
+import coverage
 
 class TestSaludar:
 
     def test_saludo1(self):
-        assert "Hola" == saludar.saludo1()
+        with coverage.start():
+            assert "Hola" == saludar.saludo1()
+
+        with coverage.stop():
+            pass
 
     def test_saludo2(self):
-        assert "Buenos días" == saludar.saludo2()
+        with coverage.start():
+            assert "Buenos días" == saludar.saludo2()
+
+        with coverage.stop():
+            pass
 
     def test_saludo3(self):
-        assert "Hola, ¿Qué tal?" == saludar.saludo3()
+        with coverage.start():
+            assert "Hola, ¿Qué tal?" == saludar.saludo3()
+
+        with coverage.stop():
+            pass
