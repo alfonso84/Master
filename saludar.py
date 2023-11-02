ccfbc1d7b9291df4527b0d386d058ce6b5dbf28c
ricardo <minsait8@renta4.es>
2023-11-02 21:38:23 +0100

    add new features

--- saludar.py
+++ saludar.py
@@ -6,14 +6,14 @@ class Saludos:
     def __init__(self):
         self.saludos = ["Hola", "Buenos días", "Hola, ¿Qué tal?"]
 
-    def get_saludo():
-        return self.saludos[0]
+    def get_saludo(self, nombre=None):
+        if nombre is None:
+            nombre = "Mundo"
+        return f"{self.saludos[0]}, {nombre}!"
 
 
 def saludar(nombre=None):
     saludos = Saludos()
-    saludo = saludos.get_saludo()
-    if nombre is None:
-        nombre = "Mundo"
-    print(f"{saludo}, {nombre}")
+    saludo = saludos.get_saludo(nombre)
+    print(saludo)
 
